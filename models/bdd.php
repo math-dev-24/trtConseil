9a62ec1b6256faeb61bc4ec5b923c52359072b3c
Mathieu <mathieu.busse@live.fr>
2022-05-06 18:04:52 +0200

    Initialisation du projet

--- models/bdd.php
+++ models/bdd.php
@@ -4,7 +4,8 @@ abstract class Bdd
 {
     private static $pdo;
 
-    private static function setBdd(){
+    private static function setBdd()
+    {
         $dbname = "recrutement";
         $identifiant = "root";
         $password = "";
@@ -16,11 +17,31 @@ abstract class Bdd
         self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     }
 
-    protected function getBdd(){
-        if(self::$pdo === null){
+    protected function getBdd()
+    {
+        if (self::$pdo === null) {
             self::setBdd();
         }
         return self::$pdo;
     }
+}
 
+//Model
+
+public function getAnnonce($email)
+{
+	//écriture de la requête dans une variable
+    $req = "SELECT * FROM offre WHERE emailUser = :email";
+	//préparation de la requête pour éviter les injections SQL -> permet au hacker de soutirer les informations de la base de données
+    $stmt = $this->getBdd()->prepare($req);
+	// Bind de(s) valeur(s) (associer une valeur a un paramètre)
+    $stmt->bindValue(':email',$email,PDO::PARAM_STR);
+	// execution de la requête SQL
+    $stmt->execute();
+	// permet de récuperer les informations des Tables dans la variable $data
+    $data = $stmt->fetchAll();
+	// toujours fermer la requête
+    $stmt->closeCursor();
+	// retour des $data au controller
+    return $data;
 }
